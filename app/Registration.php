3ca6bb6a5f96c3b5df215cf9f324ba69a3fbdd14
Ris3TwO <ris3two@gmail.com>
2020-02-17 00:31:33 -0400

    Change attributes

--- app/Registration.php
+++ app/Registration.php
@@ -19,4 +19,34 @@ class Registration extends Model
     {
         $this->attributes['distance'] = strtoupper($valor);
     }
+
+    public function setNamesAttribute($valor)
+    {
+        $this->attributes['names'] = ucwords(strtolower($valor));
+    }
+
+    public function setLastnamesAttribute($valor)
+    {
+        $this->attributes['lastnames'] = ucwords(strtolower($valor));
+    }
+
+    public function setCityAttribute($valor)
+    {
+        $this->attributes['city'] = ucwords(strtolower($valor));
+    }
+
+    public function setGenderAttribute($valor)
+    {
+        $this->attributes['gender'] = ucwords(strtolower($valor));
+    }
+
+    public function setShoesAttribute($valor)
+    {
+        $this->attributes['shoes'] = ucwords(strtolower($valor));
+    }
+
+    public function setTeamAttribute($valor)
+    {
+        $this->attributes['team'] = ucwords(strtolower($valor));
+    }
 }
