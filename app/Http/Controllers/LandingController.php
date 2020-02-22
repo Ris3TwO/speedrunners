7d1135e8d3344252369b154e4530feea099e1976
Ris3TwO <ris3two@gmail.com>
2020-02-22 16:52:41 -0400

    Fix

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -17,7 +17,24 @@ class LandingController extends Controller
     {
         $descriptions = Description::all();
 
-        return view('landing.home', compact('descriptions'));
+        return view('landing.home', [
+            'section1' => DB::table('descriptions')->where('section', '=', 'section1')->get(),
+            'section2' => DB::table('descriptions')->where('section', '=', 'section2')->get(),
+            'section3' => DB::table('descriptions')->where('section', '=', 'section3')->get(),
+            'section3_1' => DB::table('descriptions')->where([
+                ['section', '=', 'section3'],
+                ['order', '=', 'order1'],
+            ])->get(),
+            'section3_2' => DB::table('descriptions')->where([
+                ['section', '=', 'section3'],
+                ['order', '=', 'order2'],
+            ])->get(),
+            'section3_3' => DB::table('descriptions')->where([
+                ['section', '=', 'section3'],
+                ['order', '=', 'order3'],
+            ])->get(),
+            // 'descriptions' => $descriptions
+        ]);
     }
 
     public function colombia()
