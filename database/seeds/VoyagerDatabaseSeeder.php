0e7dc15c21f8ace6d420c78033416f649a34dd93
Ris3TwO <ris3two@gmail.com>
2020-02-22 16:04:24 -0400

    Added user seed

--- database/seeds/VoyagerDatabaseSeeder.php
+++ database/seeds/VoyagerDatabaseSeeder.php
@@ -24,5 +24,17 @@ class VoyagerDatabaseSeeder extends Seeder
         $this->seed('PermissionsTableSeeder');
         $this->seed('PermissionRoleTableSeeder');
         $this->seed('SettingsTableSeeder');
+
+        if (User::count() == 0) {
+            $role = Role::where('name', 'admin')->firstOrFail();
+
+            User::create([
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => Str::random(60),
+                'role_id'        => $role->id,
+            ]);
+        }
     }
 }
