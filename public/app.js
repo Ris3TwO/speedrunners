7cb9c06bfee5181d62f1f35d886510c484861108
Ris3TwO <ris3two@gmail.com>
2020-02-23 15:23:20 -0400

    Fix 225

--- public/app.js
+++ public/app.js
@@ -144,9 +144,10 @@ app.controller("controlador", function($scope, $http, $location) {
         objetive3.classList.remove("active-img")
         if (width >= 360 && width <= 400) {
             objetive1.classList.add("active-img");
-            objetive1.style.transform = "translate(-12px, 133px)"
-            objetive2.style.maxWidth = "194px"
-            objetive1.style.maxWidth = "305px"
+            objetive2.style.transform = "translate(-12px, 133px)"
+            objetive1.style.transform = "translate(68px, -132px)"
+            objetive1.style.maxWidth = "194px"
+            objetive2.style.maxWidth = "305px"
         }
         obj2.removeAttribute("style");
 
