08e1367d9d368f27c7e1e0634af698b398d16477
Ris3TwO <ris3two@gmail.com>
2020-02-23 19:15:45 -0400

    Fix issue scroll

--- resources/views/landing/colombia.blade.php
+++ resources/views/landing/colombia.blade.php
@@ -11,15 +11,15 @@
     <title>{{ setting('site.title') }}</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/landing-page.css" rel="stylesheet">
+    <link href="{{ asset('css/landing-page.css') }}" rel="stylesheet">
 
-    <script src="vendor/smooth-scroll.js"></script>
+    <script src="{{ asset('vendor/smooth-scroll.js') }}"></script>
 
     <style>
         .filter {
@@ -565,12 +565,12 @@
         </form>
     </section>
 
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-    <script src="angular.min.js"></script>
-    <script src="app.js"></script>
-    <script src="angular-route.min.js"></script>
+    <script src="<?= asset('angular.min.js') ?>"></script>
+    <script src="<?= asset('app.js') ?>"></script>
+    <script src="<?= asset('angular-route.min.js') ?>"></script>
 
     <script>
         var scroll = new SmoothScroll('a[href*="#"]');
