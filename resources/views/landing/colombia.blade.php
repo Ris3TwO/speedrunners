9a127c72e69f3ba322619577935d9b3a68aa84cd
Ris3TwO <ris3two@gmail.com>
2020-02-23 19:37:31 -0400

    Fix issue

--- resources/views/landing/colombia.blade.php
+++ resources/views/landing/colombia.blade.php
@@ -565,8 +565,8 @@
         </form>
     </section>
 
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="<?= asset('vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= asset('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
     <script src="<?= asset('angular.min.js') ?>"></script>
     <script src="<?= asset('app.js') ?>"></script>
